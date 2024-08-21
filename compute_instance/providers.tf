930a8d8639db89b0c0aa0f42d82fdcd7ad3604f5
dimon <dmitry>
2024-08-21 18:31:28 +0300

    fix

--- compute_instance/providers.tf
+++ compute_instance/providers.tf
@@ -1,16 +1,15 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "0.124.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0"
     }
   }
   required_version = ">=1.5"
-
-  local = {
-    source  = "hashicorp/local"
-    version = "2.0" 
-  }
 
   backend "s3" {
     endpoints = {
@@ -24,13 +23,5 @@ terraform {
     skip_credentials_validation = true
     skip_requesting_account_id  = true 
     skip_s3_checksum            = true
-
   }
-}
-
-provider "yandex" {
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.default_zone
-  service_account_key_file = var.authorized_key #file("../authorized_key.json")
 }
